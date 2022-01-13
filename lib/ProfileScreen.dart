62927f45966b9d55a0c59578797fe3e302ea56df
aabufayed <aabufayed2002@gmail.com>
2022-01-13 13:58:57 +0300

    simple ui app

---
+++ lib/ProfileScreen.dart
@@ -0,0 +1,253 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+            colors: [
+              Color.fromRGBO(4, 9, 35, 1),
+              Color.fromRGBO(39, 105, 171, 1),
+            ],
+            begin: FractionalOffset.bottomCenter,
+            end: FractionalOffset.topCenter,
+          )),
+        ),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 50),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(
+                        AntDesign.arrowleft,
+                        color: Colors.white,
+                      ),
+                      Icon(
+                        AntDesign.logout,
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    'Flutter Developer',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontFamily: 'Nisebuschgardens',
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    height: height * 0.43,
+                    child: LayoutBuilder(
+                      builder: (context, constraints) {
+                        double innerHeight = constraints.maxHeight;
+                        double innerWidth = constraints.maxWidth;
+                        return Stack(
+                          fit: StackFit.expand,
+                          children: [
+                            Positioned(
+                              bottom: 0,
+                              left: 0,
+                              right: 0,
+                              child: Container(
+                                height: innerHeight * 0.72,
+                                width: innerWidth,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(30),
+                                  color: Colors.white,
+                                ),
+                                child: Column(
+                                  children: [
+                                    SizedBox(
+                                      height: 80,
+                                    ),
+                                    Text(
+                                      'A.abufayed',
+                                      style: TextStyle(
+                                        color: Color.fromRGBO(39, 105, 171, 1),
+                                        fontFamily: 'Nisebuschgardens',
+                                        fontSize: 37,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 5,
+                                    ),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Column(
+                                          children: [
+                                            Text(
+                                              'Client Apps',
+                                              style: TextStyle(
+                                                color: Colors.grey[700],
+                                                fontFamily: 'Nunito',
+                                                fontSize: 25,
+                                              ),
+                                            ),
+                                            Text(
+                                              '6',
+                                              style: TextStyle(
+                                                color: Color.fromRGBO(
+                                                    39, 105, 171, 1),
+                                                fontFamily: 'Nunito',
+                                                fontSize: 25,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                        Padding(
+                                          padding: const EdgeInsets.symmetric(
+                                            horizontal: 25,
+                                            vertical: 8,
+                                          ),
+                                          child: Container(
+                                            height: 80,
+                                            width: 1.5,
+                                            decoration: BoxDecoration(
+                                              borderRadius:
+                                                  BorderRadius.circular(100),
+                                              color: Colors.grey,
+                                            ),
+                                          ),
+                                        ),
+                                        Column(
+
+                                          children: [
+                                            Text(
+                                              'Big Projects',
+                                              style: TextStyle(
+                                                color: Colors.grey[700],
+                                                fontFamily: 'Nunito',
+                                                fontSize: 25,
+                                              ),
+                                            ),
+                                            Text(
+                                              '3',
+                                              style: TextStyle(
+                                                color: Color.fromRGBO(
+                                                    39, 105, 171, 1),
+                                                fontFamily: 'Nunito',
+                                                fontSize: 25,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Positioned(
+                              top: 110,
+                              right: 20,
+                              child: Icon(
+                                AntDesign.setting,
+                                color: Colors.grey[700],
+                                size: 30,
+                              ),
+                            ),
+                            Positioned(
+                              top: 0,
+                              left: 0,
+                              right: 0,
+                              child: Center(
+                                child: Container(
+                                  child: CircleAvatar(
+                                    radius: 80,
+                                    backgroundImage: AssetImage(
+                                      'assets/img/profile.jpg',
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        );
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  Container(
+                    height: height * 0.25,
+                    width: width,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: Colors.white,
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 15),
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            'Contacts',
+                            style: TextStyle(
+                              color: Color.fromRGBO(39, 105, 171, 1),
+                              fontSize: 27,
+                              fontWeight: FontWeight.w300
+                            ),
+                          ),
+                          Divider(
+                            thickness: 2.5,
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          ListTile(
+                            minVerticalPadding: 3,
+                            leading: Icon(AntDesign.phone,size: 25,),
+                            title: Text('+905526047792',style: TextStyle(
+                              letterSpacing: 4
+                            ),),
+                          ),
+                          ListTile(
+                            minVerticalPadding: 3,
+                            leading: Icon(Icons.mail,size: 25,),
+                            title: Text('aabufayed2002@gmail.com',style: TextStyle(
+                                letterSpacing: 1
+                            ),),
+                          ),
+
+
+
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
